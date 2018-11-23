dca04a98be49b43d319942d81984d865df5ea037
Umair <umair@testpress.in>
2018-11-23 18:29:50 +0530

    Add db and migration in app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
 from decouple import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 application = Flask(config("APPLICATION_NAME"))
-SETTINGS_FILE = config("SETTINGS_FILE", "settings.local.py")
+SETTINGS_FILE = config("SETTINGS_FILE", "settings/local.py")
 application.config.from_pyfile(SETTINGS_FILE)
+
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)

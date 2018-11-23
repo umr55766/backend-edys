e9d58ec6f7ff43670bfc78d378c0e1e7eb0b7b7c
Umair <umair@testpress.in>
2018-11-23 18:25:09 +0530

    Refactor settings in setting file

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-import os
-
 from flask import Flask
 
 from decouple import config
 
 application = Flask(config("APPLICATION_NAME"))
+SETTINGS_FILE = config("SETTINGS_FILE", "settings.local.py")
+application.config.from_pyfile(SETTINGS_FILE)

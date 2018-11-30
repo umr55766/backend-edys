0780813fc3ce8cf210437d14d3014a5e96c98139
Umair <umair@testpress.in>
2018-11-30 18:29:47 +0530

    Expose all the objects

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 import requests
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 
 from decouple import config
-from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_rq2 import RQ
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import URLType
+
 
 application = Flask(config("APPLICATION_NAME"))
 SETTINGS_FILE = config("SETTINGS_FILE", "settings/local.py", cast=str)
@@ -17,6 +19,7 @@ application.config.from_pyfile(SETTINGS_FILE)
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 rq = RQ(application)
+ma = Marshmallow(application)
 
 
 @rq.job
@@ -29,14 +32,28 @@ def count_words_task(url):
         print("Saved successfully")
 
 
-@application.route("/")
+@application.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == "GET":
+        page_word_counts = PageWordCount.query.all()
+        serializer = page_word_counts_serializer.dump(page_word_counts)
+        return jsonify(serializer.data)
+
     url = request.args.get('url')
-    count_words_task.queue(url)
-    return count_words_task.queue(url).id
+    task = count_words_task.queue(url)
+    return jsonify({"task_id": task.id})
 
 
 class PageWordCount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(URLType)
     word_count = db.Column(db.Integer)
+
+
+class PageWordCountSerializer(ma.Schema):
+    class Meta:
+        fields = ('id', 'url', 'word_count')
+
+
+page_word_count_serializer = PageWordCountSerializer()
+page_word_counts_serializer = PageWordCountSerializer(many=True)
